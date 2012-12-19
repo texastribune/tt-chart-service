59f97c19adae1568b6accefcb26d292a07172108
Noah Seger <nosamanuel@gmail.com>
2012-12-19 14:45:46 -0600

    Only accept SVG data via POST

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from subprocess import Popen, PIPE, STDOUT
 
-from flask import Flask, Response
+from flask import Flask, Response, request, abort
 from flask_heroku import Heroku
 
 app = Flask(__name__)
 heroku = Heroku(app)
 
 
-@app.route('/render/')
+@app.route('/render/', methods=['POST'])
 def render():
-    svg_bin = """<svg width="200px" height="100px">
-<circle cx="100" cy="50" r="40" stroke="black" stroke-width="2" fill="red" />
-</svg>"""
+    svg = request.form.get('svg')
+    if not svg:
+        abort(400)
     command = ['convert', 'svg:-', 'png:-']
     process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    png_bin = process.communicate(input=svg_bin)[0]
+    png_bin = process.communicate(input=svg)[0]
     return Response(png_bin, mimetype='image/png')
 
 
