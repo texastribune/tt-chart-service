117a80afc93473f61316f278a493baadd3f5b963
Noah Seger <nosamanuel@gmail.com>
2012-12-19 13:51:18 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from subprocess import Popen, PIPE, STDOUT
+
+from flask import Flask, Response
+
+
+app = Flask(__name__)
+
+
+@app.route('/render/')
+def render():
+    svg_bin = """<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+<circle cx="100" cy="50" r="40" stroke="black" stroke-width="2" fill="red" />
+</svg>"""
+    command = ['convert', 'svg:-', 'png:-']
+    process = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    png_bin = process.communicate(input=svg_bin)[0]
+    return Response(png_bin, mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run()
